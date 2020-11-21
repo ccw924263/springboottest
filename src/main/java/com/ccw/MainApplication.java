96b4fa2db734e5294fc93c92bf0d05046f110a12
ccw924263 <ccw924263059>
2020-11-21 16:34:38 +0800

    spring

--- src/main/java/com/ccw/MainApplication.java
+++ src/main/java/com/ccw/MainApplication.java
@@ -1,12 +1,16 @@
 package com.ccw;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@ComponentScans({})
+@MapperScan(basePackages = "com.ccw")
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
